a265b284eefb79e9de53cce8f54bbb0cf212f692
Jens <107625279+Jensaboi@users.noreply.github.com>
2025-08-30 00:33:35 +0200

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import tmdbLogo from "../assets/tmdb-logo.png";
 export default function Footer() {
   return (
-    <footer>
-      <p>My footer</p>
+    <footer className="p-10 flex-center gap-lg bg-bg-400">
+      <p>
+        Powered by The Movie Database{" "}
+        <a
+          className="text-blue-400 hover:underline"
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener"
+        >
+          (TMDB)
+        </a>
+      </p>
+      <a
+        className="hover:brightness-120 hover:scale-101"
+        href="https://www.themoviedb.org/"
+        target="_blank"
+        rel="noopener"
+      >
+        <img width="40" src={tmdbLogo} alt="TMDb Logo" />
+      </a>
     </footer>
   );
 }
