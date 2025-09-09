758c71deed128afae001dedb99358b724e6ec233
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-09-09 14:46:24 +0200

    whitespace

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -11,7 +11,7 @@ export default function Button({
     solid:
       "bg-bg-300 shadow-2xl text-text-300 hover:text-text-100 hover:bg-bg-400 active:bg-bg-200 inline-flex items-center justify-center gap-xs rounded-full transistion-all ease-in duration-300 shadow-lg hover:scale-101",
     ghost: "",
-    page: "border-1 border-bg-400 size-12 inline-flex justify-center items-center",
+    page: "border-1 border-bg-400 size-12 inline-flex justify-center items-center ",
     carosuel:
       "top-1/3 inline-flex items-center justify-center text-lg absolute z-2 p-xs bg-bg-300/60 hover:bg-bg-300/90 transistion-all duration-300 ease-in-out rounded-md border border-transparent hover:border-accent/50 w-12 h-22",
   };
